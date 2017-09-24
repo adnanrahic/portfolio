c8d36383009a5b394d5df61180a8405c95c7f6e1
adnanrahic <ado.raha198@gmail.com>
2017-09-24 19:26:08 +0200

    Added routes, main portfolio page, sass, and sass variables.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { DataService } from './data.service';
   selector: 'app-root',
   providers: [DataService],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MaterialModule } from './material/material.module';
+import { RoutesModule } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    RoutesModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
